62788ef226aff4f90a7f74bd45a282aa8e11c1c4
flow3flow <floramine75@hotmail.com>
2022-10-05 11:49:28 +0200

    Hello world

---
+++ 01-hello-word.php
@@ -0,0 +1,24 @@
+<?php
+//commentaire sur une ligne
+
+//on peut mettre des balises php n'importe où dans une page html
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <?php // ouverture de PHP
+    echo "<h1>Hello World</h1>";
+
+   // fermeture de PHP ?>
+    
+</body>
+</html>
