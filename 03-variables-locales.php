f96845c56a312f3fe29eea35dab96dac5027f39d
flow3flow <floramine75@hotmail.com>
2022-10-05 13:44:24 +0200

    variables locales

---
+++ 03-variables-locales.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variables locales</title>
+</head>
+<body>
+    <h1>variables locales</h1>
+    <p>En PHP, les variables sont représentées par un signe dollar "$" suivi du nom de la variable. Le nom est sensible à la casse.
+
+Les noms de variables suivent les mêmes règles de nommage que les autres entités PHP. Un nom de variable valide doit commencer par une lettre ou un souligné (_),
+ suivi de lettres, chiffres ou soulignés</p>
+ <p>On va éviter les signes servant aux calculs (+-*/)</p>
+
+    <h2>Utilité des variables</h2>
+    <p>Elles servent à stocker des informations qui peuvent être manipulées, affichées , sauvegardées, détruites...</p>
+    <p>A noter qu'en php dès que l'on arrive en bas de page les donnés sont détruites.</p>
+</body>
+</html>
