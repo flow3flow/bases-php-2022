b52a0f3c2be41dd34a198f9959c2b3579744f8a0
flow3flow <floramine75@hotmail.com>
2022-10-27 15:53:16 +0200

    exoflo

--- 20-cf-exe/pages/formaweb.php
+++ 20-cf-exe/pages/formaweb.php
@@ -9,12 +9,12 @@
 </head>
 <body>
     <h1>Formateurs WEB</h1>
-    <ul class="nav">
+   <!-- <ul class="nav">
         <li><a href="./">Accueil 20</a>
         <li><a href="?u=formateurs">Formateurs WEB</a></li>
         <li><a href="?u=stagiaires">Stagiaires WEB</a></li>
         <li><a href="?u=classes">Classes CF2m</a></li>
-    </ul>
+    </ul> -->
     <container>
         <h2>Liste des formateurs WEB</h2>
         <?php
